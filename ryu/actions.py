f1f3c206ea9ce89ecbdfbe6b614f8e225bf5f958
clemon <cjw7360chen@gmail.com>
2022-04-08 21:09:15 +0800

    impl actions

--- ryu/actions.py
+++ ryu/actions.py
@@ -33,18 +33,33 @@ def add_private_flow(datapath, priority=50):
         mod = build_flow(datapath, priority, actions, match)
         datapath.send_msg(mod)
     
-# ??????
-def add_dns_proxy_flow(datapath, ips=[], macs=[], priority=100):
+def add_dns_proxy_flow(datapath, hosts, priority=100):
+    """proxy dns of special hosts
+       ignore hosts' dns server.
+
+    Args:
+        datapath (datapath): ryu datapath
+        hosts (list): specific host's ip or mac. 
+                      At least one of 'ip' or 'mac' is not empty. 
+                      If both 'ip' and 'mac' are all inputed, 'mac' has higher priority than 'ip'.
+                      e.g. [
+                            {"ip"="10.0.0.1", "mac"="76:7e:26:77:72:4b"},
+                            {"ip"="10.0.0.2", "mac"="88:7e:26:77:72:43"},
+                            {"ip"="10.0.0.3"},
+                            {"mac"="88:7e:26:77:72:40"}
+                           ]
+        priority (int, optional): priority. Defaults to 100.
+    """
     ofproto = datapath.ofproto
     parser = datapath.ofproto_parser
     
-    # ipv4_src = private ips
+    # ipv4_src = private ips or eth_src = private macs
     req_match = dict(
             eth_type=ether_types.ETH_TYPE_IP,
             ip_proto=17,
             udp_dst=53)
     
-    # ipv4_dst = private ips
+    # ipv4_dst = private ips or eth_dst = private macs
     resp_match = dict(
             eth_type=ether_types.ETH_TYPE_IP,
             ip_proto=17,
@@ -53,17 +68,30 @@ def add_dns_proxy_flow(datapath, ips=[], macs=[], priority=100):
     actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, 
                                         ofproto.OFPCML_NO_BUFFER)]
     
-    for ip in ips:
-        match.update(ipv4_src=str(ip))
-        mod = build_flow(datapath, priority, actions, match)
-        datapath.send_msg(mod)
+    for host in hosts:
         
-    del match["ipv4_src"]
-    for mac in macs:
-        match.update(eth_src=str(mac))
-        mod = build_flow(datapath, priority, actions, match)
-        datapath.send_msg(mod)
-
+        if host.has_key("mac"):
+            # req
+            req_match.update(eth_src=str(host["mac"]))
+            mod = build_flow(datapath, priority, actions, req_match)
+            datapath.send_msg(mod)
+            del req_match["eth_src"]
+            # resp
+            resp_match.update(eth_dst=str(host["mac"]))
+            mod = build_flow(datapath, priority, actions, resp_match)
+            datapath.send_msg(mod)
+            del resp_match["eth_dst"]
+        else:
+            # req
+            req_match.update(ipv4_src=str(host["ip"]))
+            mod = build_flow(datapath, priority, actions, req_match)
+            datapath.send_msg(mod)
+            del req_match["ipv4_src"]
+            # resp
+            resp_match.update(ipv4_dst=str(host["ip"]))
+            mod = build_flow(datapath, priority, actions, resp_match)
+            datapath.send_msg(mod)
+            del resp_match["ipv4_dst"]
 
 def build_flow(datapath, priority, actions=[], match={}, **kwargs):
     ofproto = datapath.ofproto

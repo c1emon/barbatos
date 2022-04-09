5829a661a5a3f989198690138a565ed35790831b
clemon <cjw7360chen@gmail.com>
2022-04-09 16:09:00 +0800

    init dns pkg parser

--- ryu/dns.py
+++ ryu/dns.py
@@ -1,13 +1,15 @@
 from ryu.lib.packet import packet_base
-
+import struct
 class dns(packet_base.PacketBase):
+    _DNS_PACK_HEADER = "!HBBHHHH"
+    _MIN_LEN = struct.calcsize(_DNS_PACK_HEADER)
     
     def __init__(self):
         super(dns, self).__init__()
         
     @classmethod
     def parser(cls, buf):
-        pass
+        print(struct.unpack_from(cls._DNS_PACK_HEADER, buf))
     
     def serialize(self, _payload=None, _prev=None):
         pass
--- ryu/traffic_processor.py
+++ ryu/traffic_processor.py
@@ -52,81 +52,81 @@ class Tproxy(app_manager.RyuApp):
         add_private_flow(datapath)
         add_normal_flow(datapath)
    
-    # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    # def packet_in_handler(self, ev):
-    #     msg = ev.msg
-    #     datapath = msg.datapath
-    #     ofproto = datapath.ofproto
-    #     parser = datapath.ofproto_parser
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         
-    #     p = packet.Packet(msg.data)
+        p = packet.Packet(msg.data)
         
-    #     eth_pkg = p.get_protocol(ethernet.ethernet)
-    #     ipv4_pkg = p.get_protocol(ipv4.ipv4)
-    #     tcp_pkg = p.get_protocol(tcp.tcp)
-    #     udp_pkg = p.get_protocol(udp.udp)
-    #     # TODO: assert ipv4_pkg not None
-    #     pkg = tcp_pkg if tcp_pkg else udp_pkg
-    #     # body = eth_pkg.protocols[-1]
+        eth_pkg = p.get_protocol(ethernet.ethernet)
+        ipv4_pkg = p.get_protocol(ipv4.ipv4)
+        tcp_pkg = p.get_protocol(tcp.tcp)
+        udp_pkg = p.get_protocol(udp.udp)
+        # TODO: assert ipv4_pkg not None
+        pkg = tcp_pkg if tcp_pkg else udp_pkg
+        # body = eth_pkg.protocols[-1]
 
-    #     if udp_pkg and pkg.dst_port == 53 and ipv4_pkg.src in self.proxy_ips:
-    #         # dns req
-    #         # hack to proxy:53
-    #         self.logger.info("dns request(%s -> %s): %s", ipv4_pkg.src, ipv4_pkg.dst, udp_pkg.data)
-    #         actions = [
-    #             parser.OFPActionSetField(eth_dst=PROXY_GW["mac"]),
-    #             parser.OFPActionSetField(ipv4_dst=str(PROXY_GW["ip"])),
-    #             parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
-    #         out = parser.OFPPacketOut(
-    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-    #             actions=actions, data=p)
-    #         datapath.send_msg(out)
-    #         return
+        if udp_pkg and pkg.dst_port == 53 and ipv4_pkg.src in self.proxy_ips:
+            # dns req
+            # hack to proxy:53
+            self.logger.info("dns request(%s -> %s): %s", ipv4_pkg.src, ipv4_pkg.dst, udp_pkg.data)
+            actions = [
+                parser.OFPActionSetField(eth_dst=PROXY_GW["mac"]),
+                parser.OFPActionSetField(ipv4_dst=str(PROXY_GW["ip"])),
+                parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
+            out = parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+                actions=actions, data=p)
+            datapath.send_msg(out)
+            return
         
-    #     if udp_pkg and pkg.src_port == 53 and ipv4_pkg.src == str(PROXY_GW["ip"]) and ipv4_pkg.dst in self.proxy_ips:
-    #         self.logger.info("dns(p -> h): %s", udp_pkg)
-    #         actions = [
-    #             parser.OFPActionSetField(eth_src=DEFAULT_GW["mac"]),
-    #             parser.OFPActionSetField(ipv4_src="114.114.114.114"),
-    #             parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
-    #         out = parser.OFPPacketOut(
-    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-    #             actions=actions, data=p)
-    #         datapath.send_msg(out)
-    #         return
+        if udp_pkg and pkg.src_port == 53 and ipv4_pkg.src == str(PROXY_GW["ip"]) and ipv4_pkg.dst in self.proxy_ips:
+            self.logger.info("dns(p -> h): %s", udp_pkg)
+            actions = [
+                parser.OFPActionSetField(eth_src=DEFAULT_GW["mac"]),
+                parser.OFPActionSetField(ipv4_src="114.114.114.114"),
+                parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
+            out = parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+                actions=actions, data=p)
+            datapath.send_msg(out)
+            return
         
-    #     if ipv4_pkg.src in self.proxy_ips and is_public(ipv4_pkg.dst):
-    #         # TODO: host -> proxy
+        if ipv4_pkg.src in self.proxy_ips and is_public(ipv4_pkg.dst):
+            # TODO: host -> proxy
             
-    #         self.logger.info("%s: %s(%s) ---> %s[(%s) map to (%s)]", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, ipv4_pkg.dst, eth_pkg.dst, PROXY_GW["mac"])
-    #         actions = [
-    #             parser.OFPActionSetField(eth_dst=PROXY_GW["mac"]), 
-    #             parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
-    #         out = parser.OFPPacketOut(
-    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-    #             actions=actions, data=p)
-    #         datapath.send_msg(out)
-    #         return
+            self.logger.info("%s: %s(%s) ---> %s[(%s) map to (%s)]", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, ipv4_pkg.dst, eth_pkg.dst, PROXY_GW["mac"])
+            actions = [
+                parser.OFPActionSetField(eth_dst=PROXY_GW["mac"]), 
+                parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
+            out = parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+                actions=actions, data=p)
+            datapath.send_msg(out)
+            return
         
-    #     if ipv4_pkg.dst in self.proxy_ips and is_public(ipv4_pkg.src):
-    #         # TODO: proxy -> host
+        if ipv4_pkg.dst in self.proxy_ips and is_public(ipv4_pkg.src):
+            # TODO: proxy -> host
             
-    #         self.logger.info("%s: %s[(%s) map to (%s)] ---> %s(%s)", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, DEFAULT_GW["mac"], ipv4_pkg.dst, eth_pkg.dst)
-    #         actions = [
-    #             parser.OFPActionSetField(eth_src=DEFAULT_GW["mac"]), 
-    #             parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
-    #         out = parser.OFPPacketOut(
-    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-    #             actions=actions, data=p)
-    #         datapath.send_msg(out)
-    #         return
+            self.logger.info("%s: %s[(%s) map to (%s)] ---> %s(%s)", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, DEFAULT_GW["mac"], ipv4_pkg.dst, eth_pkg.dst)
+            actions = [
+                parser.OFPActionSetField(eth_src=DEFAULT_GW["mac"]), 
+                parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
+            out = parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+                actions=actions, data=p)
+            datapath.send_msg(out)
+            return
         
-    #     # NORMAL ACTION
-    #     self.logger.info("Local: %s: %s(%s) ---> %s(%s)", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, ipv4_pkg.dst, eth_pkg.dst)
-    #     actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
-    #     out = parser.OFPPacketOut(
-    #         datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
-    #         actions=actions, data=p)
-    #     datapath.send_msg(out)
+        # NORMAL ACTION
+        self.logger.info("Local: %s: %s(%s) ---> %s(%s)", pkg.protocol_name, ipv4_pkg.src, eth_pkg.src, ipv4_pkg.dst, eth_pkg.dst)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL, ofproto.OFPCML_NO_BUFFER)]
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match["in_port"],
+            actions=actions, data=p)
+        datapath.send_msg(out)
         
         

5947f0b8165ca6666a0f0238eb303f66888068dc
clemon <cjw7360chen@gmail.com>
2022-06-13 13:28:20 +0800

    add stop

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ Restart=on-failure
 
 ExecStart=ryu-manager /etc/ryu_app/traffic_processor.py
 ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s HUP $MAINPID
 KillMode=process
 TimeoutStopSec=5
 
